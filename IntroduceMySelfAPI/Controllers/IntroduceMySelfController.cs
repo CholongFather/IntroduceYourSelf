5bce9d2c3e72b0edc018a71548683dd7c1868a07
Knh <kms1844@fxgear.com>
2021-03-12 17:36:25 +0900

    api set & swagger

--- IntroduceMySelfAPI/Controllers/IntroduceMySelfController.cs
+++ IntroduceMySelfAPI/Controllers/IntroduceMySelfController.cs
@@ -27,10 +27,64 @@ namespace IntroduceMySelfAPI.Controllers
             _redisCacheClient = redisCacheClient;
         }
 
-        [HttpGet]
-        public async ValueTask<string> Get()
+        /// <summary>
+        /// 이름 데이터
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("MyName")]
+        public async ValueTask<string> GetName()
         {
             return await _redisCacheClient.GetDbFromConfiguration().GetAsync<string>("MyName");
+        }
+
+        /// <summary>
+        /// 소개데이터
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("Introduce")]
+        public async ValueTask<Introduce> GetIntroduce()
+        {
+            return await _redisCacheClient.GetDbFromConfiguration().GetAsync<Introduce>("SangWan");
+        }
+
+        /// <summary>
+        /// 기본 데이터 셋
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("SetSangWan")]
+        public async ValueTask PostIntroduce()
+        {
+            var sangWan = new Introduce();
+            sangWan.Author = new Author()
+            {
+                Name = "Park Sang Wan",
+                Age = 99,
+                CareerYear = (DateTime.Now - new DateTime(1990,1,1)).Days / 365,
+            };
+            sangWan.Carrer = new List<ProfessionalExperience>();
+
+
+            ///Json 파일 읽어서 하는걸로 바꿀것.
+            var professionalExperience = new ProfessionalExperience()
+            {
+                ProjectName = "참좋은여행",
+                Company = "참좋은여행",
+                ImageLink = "#",
+                SkillInventory = new List<string> { "Jquery", "ASP.NET", "반응형" },
+            };
+
+            ProfessionalExperience professionalExperience2 = new ProfessionalExperience()
+            {
+                ProjectName = "참좋은여행 불꽃폭발",
+                Company = "참망한여행",
+                ImageLink = "#",
+                SkillInventory = new List<string> { "Jquery 1.12.4", "반응형" },
+            };
+
+            sangWan.Carrer.Add(professionalExperience);
+            sangWan.Carrer.Add(professionalExperience2);
+
+            await _redisCacheClient.GetDbFromConfiguration().AddAsync<Introduce>("SangWan", sangWan);
         }
     }
 }
