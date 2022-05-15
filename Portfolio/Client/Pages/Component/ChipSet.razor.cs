dbc4f606cc0d16236af351bc92fda50caf9dfb64
CPH <kms1844@naver.com>
2022-05-15 19:51:43 +0900

    Component Chips / ChipSet Implemet

---
+++ Portfolio/Client/Pages/Component/ChipSet.razor.cs
@@ -0,0 +1,25 @@
+﻿using MudBlazor;
+
+namespace Portfolio.Client.Pages.Component;
+
+public partial class ChipSet
+{
+	private bool _mandatory { get; set; } = false;
+
+	private bool _filter { get; set; } = false;
+
+	private Size _size { get; set; } = Size.Small;
+
+	private Variant _variant { get; set; } = Variant.Filled;
+
+	private Color _selectedColor { get; set; } = Color.Primary;
+
+
+	private MudChip selectedMudchip;
+
+	private MudChip[] selectedMudchips;
+
+	protected override async Task OnInitializedAsync()
+	{
+	}
+}
---
+++ Portfolio/Client/Pages/Component/Chips.razor.cs
@@ -0,0 +1,14 @@
+﻿using MudBlazor;
+
+namespace Portfolio.Client.Pages.Component;
+
+public partial class Chips
+{
+	private Size _size { get; set; } = Size.Small;
+
+	private Variant _variant { get; set; } = Variant.Filled;
+
+	protected override async Task OnInitializedAsync()
+	{
+	}
+}
