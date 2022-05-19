666219a72077882224b9bbee46154e59f5472e5f
CPH <kms1844@naver.com>
2022-05-20 04:26:25 +0900

    Chart Implement

---
+++ Portfolio/Client/Pages/Component/Chart/BarChart.razor.cs
@@ -0,0 +1,40 @@
+﻿namespace Portfolio.Client.Pages.Component.Chart;
+
+public partial class BarChart
+{
+	private ChartOptions _chartOptions = new();
+	private List<ChartSeries> _series = new();
+
+	private string[] _xAxisLabels = { "1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월" };
+	private Random _random = new();
+	private int _index = -1;
+
+	protected override async Task OnInitializedAsync()
+	{
+		_chartOptions.InterpolationOption = InterpolationOption.NaturalSpline;
+		_chartOptions.YAxisFormat = "c2";
+
+		await RandomizeData();
+	}
+
+	private async Task RandomizeData()
+	{
+		var new_series = new List<ChartSeries>()
+		{
+			new() { Name = "범례 1", Data = new double[12] },
+			new() { Name = "범례 2", Data = new double[12] },
+			new() { Name = "범례 3", Data = new double[12] },
+		};
+
+		for (var i = 0; i < 11; i++)
+		{
+			new_series[0].Data[i] = _random.NextDouble() * 100;
+			new_series[1].Data[i] = _random.NextDouble() * 100;
+			new_series[2].Data[i] = _random.NextDouble() * 100;
+		}
+
+		_series = new_series;
+
+		await InvokeAsync(StateHasChanged);
+	}
+}
---
+++ Portfolio/Client/Pages/Component/Chart/PieChart.razor.cs
@@ -0,0 +1,47 @@
+﻿namespace Portfolio.Client.Pages.Component.Chart;
+
+public partial class PieChart
+{
+	private int _selectedIndex = -1;
+	private int _dataSize = 4;
+	public double[] _chartData;
+	public string[] _chartLabels = { "1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월" };
+
+	Random random = new();
+
+	protected override async Task OnInitializedAsync()
+	{
+		await RandomizeData();
+	}
+
+	private async Task RandomizeData()
+	{
+		var data = new double[_dataSize];
+
+		for (var i = 0; i < data.Length; i++)
+			data[i] = random.NextDouble() * 100;
+
+		_chartData = data;
+
+		await InvokeAsync(StateHasChanged);
+	}
+
+	private async Task AddDataSize()
+	{
+		if (_dataSize < 12)
+		{
+			_dataSize = _dataSize + 1;
+
+			await RandomizeData();
+		}
+	}
+	private async Task RemoveDataSize()
+	{
+		if (_dataSize > 0)
+		{
+			_dataSize = _dataSize - 1;
+
+			await RandomizeData();
+		}
+	}
+}
